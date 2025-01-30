35a660fd91cc9516c75695a7422fe7312b7103af
Mykola Mykhalov <mmykhalov@atlassian.com>
2025-01-30 10:51:43 +0100

    Init

--- tools/function_tool/main.py
+++ tools/function_tool/main.py
@@ -18,7 +18,7 @@ def test_123(
     import os
     import base64
 
-    print(f"Hello {name}! Secret: {base64.b64encode(os.environ.get('TEST_SECRET', 'no secret'))}. {boolean_val} {optional_str}")
+    print(f"Hello {name}! Secret: {base64.b64encode(os.environ.get('TEST_SECRET', 'no secret').encode('ascii'))}. {boolean_val} {optional_str}")
     df = pd.DataFrame({"name": [name], "boolean_val": [boolean_val], "test": [optional_str]})
 
     print(df)
