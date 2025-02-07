3605fee69a0712faab227e87dfb86dd54e529a16
Mykola Mykhalov <mmykhalov@atlassian.com>
2025-02-07 11:32:23 +0100

    Init

--- tools/function_tool/main.py
+++ tools/function_tool/main.py
@@ -4,26 +4,6 @@ from typing_extensions import Annotated
 
 
 @function_tool(
-    description="Prints pandas {name}!",
-    requirements=["pandas==2.2.3"],
-    secrets=["TEST_SECRET"],
-)
-def test_123(
-    name: str,
-    boolean_val: bool,  # This will validate that the input is a boolean
-    optional_str: Annotated[str, typer.Argument()] = "sheeesh",  # This is how to add a default value
-):
-    import pandas as pd
-    import os
-    import base64
-
-    print(f"Hello {name}! Request user: {os.environ.get('KUBIYA_USER_EMAIL', 'no user')}. Secret: {base64.b64encode(os.environ.get('TEST_SECRET', 'no secret').encode('ascii'))}. {boolean_val} {optional_str}")
-    df = pd.DataFrame({"name": [name], "boolean_val": [boolean_val], "test": [optional_str]})
-
-    print(df)
-
-
-@function_tool(
     description="Get failed environment logs from InstEnv",
     requirements=["requests==2.26.0"],
     secrets=["INSTENV_API_TOKEN"],
@@ -84,8 +64,12 @@ def get_instenv_logs(
 
 
     instenv = InstEnvHelper(environment_id)
+    env_configuration = instenv.get_environment_configuration()
     last_logs = instenv.get_run_logs()
 
+    print("Environment configuration:")
+    print(env_configuration)
+    print("Last failed run logs:")
     print(last_logs)
 
 
